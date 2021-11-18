4128d75debee151a754fbe68ea6cb1e56eda6eaf
Isela <isela.v.alarcon@gmail.com>
2021-11-18 18:44:28 +0100

    starting the react project number 6

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//IMPORT COMPONENTS
+import Header from "./layout/header/Header";
+
+//IMPORT STYLES
+import "../styles/reset.css";
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+export default App;
---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+//IMPORT STYLES
+import { HeaderContainer } from "./Header.styles";
+
+const Header = () => {
+  return <HeaderContainer>alo</HeaderContainer>;
+};
+
+export default Header;
