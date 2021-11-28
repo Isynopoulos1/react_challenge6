214e382d2ff37bcf74fa96eb9d915a12c893c78c
Isela <isela.v.alarcon@gmail.com>
2021-11-28 16:56:10 -0600

    fixing the input part 3

--- src/components/elements/Input/Input.js
+++ src/components/elements/Input/Input.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { SearchBar, InputWrapper } from "./Input.styles";
+import { SearchBar, InputWrapper, InputList } from "./Input.styles";
 
 const Input = () => {
   return (
@@ -9,6 +9,13 @@ const Input = () => {
         placeholder="Adéntrate en el mundo creativo mientras trabajas"
         name="name"
       />
+      <InputList>
+        <ul>Proyectos</ul>
+        <ul>Imágenes</ul>
+        <ul>Prototipos</ul>
+        <ul>Personas</ul>
+        <ul>Tendencias</ul>
+      </InputList>
     </InputWrapper>
   );
 };
--- src/components/elements/Input/Input.styles.js
+++ src/components/elements/Input/Input.styles.js
@@ -5,20 +5,28 @@ export const InputWrapper = styled.div`
   width: 100%;
   border-radius: 30px;
   border: 1px solid #e8e8e8;
+  justify-content: center;
 `;
 export const SearchBar = styled.input`
   display: flex;
+  width: 100%;
   align-items: center;
   justify-content: center;
-  width: 100%;
   font-size: 16px;
   border: none;
   padding 10px;
   border-radius: 30px;
 `;
 
-export const nav = styled.nav`
-  width: 100%;
+export const InputList = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 518px;
+  border-left: 1px solid #e8e8e8;
+  height: auto;
   font-size: 10px;
+  font-family: helvetica;
+  grid-gap: 20px;
+  font-weight: bold;
 `;
