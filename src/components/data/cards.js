eb850384298eea3d841360f3b6c7cdf84195f9e7
Isela <isela.v.alarcon@gmail.com>
2021-12-02 12:55:41 -0600

    frixing the main component

--- src/components/data/cards.js
+++ src/components/data/cards.js
@@ -3,24 +3,24 @@ export const list1 = [
     image:
       "https://ik.imagekit.io/ppayaz/react_challenge3/bench-accounting-nvzvOPQW0gc-unsplash_l7BgEkVcT.jpeg?updatedAt=1636128123237",
     title: "Aldus PageMaker",
-    author: "30m²"
+    author: "fdfd²"
   },
   {
     image:
       "https://ik.imagekit.io/ppayaz/react_challenge3/kam-idris-_HqHX3LBN18-unsplash_maHESzct_.jpeg?updatedAt=1636407336870",
     title: "Richard McClintock",
-    author: "37m²"
+    author: "dffd²"
   },
   {
     image:
       "https://ik.imagekit.io/ppayaz/react_challenge3/sven-brandsma-GmRiN7tVW1w-unsplash_NwgTrwvGXA.jpeg?updatedAt=1636407346133",
     title: "Malorum",
-    author: "80m²"
+    author: "wre"
   },
   {
     image:
       "https://ik.imagekit.io/ppayaz/react_challenge3/andy-art-zwZpdhoTbU0-unsplash_bQYE-PLO7.jpeg?updatedAt=1636407272596",
     title: "Cicero",
-    author: "34m²"
+    author: "fgdgj"
   }
 ];
--- src/components/layout/main/Main.styles.js
+++ src/components/layout/main/Main.styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   display: flex;
+  width: 100%;
   justify-content: center;
   align-items: center;
   right: 0;
